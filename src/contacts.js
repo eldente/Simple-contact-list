de927a6f6311a22e8a0892e935e179bf1cc36150
Ivo <ivo.zubcic@gmail.com>
2021-02-04 18:09:37 +0100

    first commit

---
+++ src/contacts.js
@@ -0,0 +1,28 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Elon Musk",
+    imgURL:
+      "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f47d4de7637290765bce495%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1398%26cropX2%3D3908%26cropY1%3D594%26cropY2%3D3102",
+    phone: "+123 456 789",
+    email: "e@elonmusk.com"
+  },
+  {
+    id: 2,
+    name: "Jack Bauer",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    id: 3,
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
+
+export default contacts;
